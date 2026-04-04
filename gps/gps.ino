ae6fffbb5ac0d8d85088c47ebbb66722755e9639
kuchaw <lzcn.inaki@gmail.com>
2026-04-04 18:55:59 -0300

    codigo labeed "gps" es el que se sube al esp32 de tierra!! puto el que lee

--- gps/gps.ino
+++ gps/gps.ino
@@ -1,29 +1,80 @@
 #include <TinyGPSPlus.h>
-
+#include <WiFi.h>
+#include <HTTPClient.h>
 
 TinyGPSPlus gps;
 #define gpsSerial Serial2
 
+//  WiFi credentials
+const char* ssid = "Fernandez";
+const char* password = "16111505";
+
+// Server endpoint (CHANGE THIS)
+const char* serverUrl = "http://192.168.0.101:3000/api/telemetry";
 
 void setup() {
   Serial.begin(115200);
   gpsSerial.begin(9600, SERIAL_8N1, 16, 17);
+
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi connected!");
   Serial.println("Waiting for GPS fix and satellites...");
 }
 
 void loop() {
 
-  while (gpsSerial.available() > 0)
-    if (gps.encode(gpsSerial.read()))
+  while (gpsSerial.available() > 0) {
+    if (gps.encode(gpsSerial.read())) {
       displayLocationInfo();
+      sendToServer();   
+    }
+  }
 
   if (millis() > 5000 && gps.charsProcessed() < 10) {
     Serial.println(F("No GPS detected: check wiring."));
     while (true);
   }
 
-
   delay(1000);
+}
+
+void sendToServer() {
+  // only send if GPS is valid
+  if (!gps.location.isValid()) {
+    Serial.println("GPS not valid, skipping send");
+    return;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/json");
+
+    // build JSON
+    String json = "{";
+    json += "\"lat\":" + String(gps.location.lat(), 6) + ",";
+    json += "\"lon\":" + String(gps.location.lng(), 6) + ",";
+    json += "\"alt\":" + String(gps.altitude.meters()) + ",";
+    json += "\"sat\":" + String(gps.satellites.value());
+    json += "}";
+
+    int code = http.POST(json);
+
+    Serial.print("HTTP Response: ");
+    Serial.println(code);
+
+    http.end();
+  } else {
+    Serial.println("WiFi disconnected");
+  }
 }
 
 void displayLocationInfo() {
@@ -73,4 +124,5 @@ void displayLocationInfo() {
   }
 
   Serial.println(F("-------------------------------------"));
+  delay(5000);
 }
