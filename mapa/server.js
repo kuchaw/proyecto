406c1e267982b2a6e51b0f7eedf013932f04c9ee
kuchaw <lzcn.inaki@gmail.com>
2026-04-04 18:02:46 -0300

    new file:   mapa/mapa.html

---
+++ mapa/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(__dirname));
+
+let latestTelemetry = null;
+
+
+app.post("/api/telemetry", (req, res) => {
+  latestTelemetry = req.body;
+  console.log("Received:", latestTelemetry);
+  res.sendStatus(200);
+});
+
+app.get("/api/telemetry", (req, res) => {
+  res.json(latestTelemetry || null);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Running on port", PORT);
+});
