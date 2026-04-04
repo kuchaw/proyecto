79417020224e22d165c97cc06a1c54291155c125
kuchaw <lzcn.inaki@gmail.com>
2026-04-04 19:43:54 -0300

    still debugging for the onrender deployment

--- mapa/server.js
+++ mapa/server.js
@@ -38,6 +38,10 @@ app.get("/api/telemetry", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/mapa.html");
+});
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Running on port", PORT);
 });
