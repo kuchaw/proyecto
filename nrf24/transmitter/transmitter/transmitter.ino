d6c40dac11f5c22baf719acb25f1ad51f9190c28
kuchaw <lzcn.inaki@gmail.com>
2026-05-07 18:25:37 -0300

    eliminacion de los sensores ds y mpu puesto que no van a ser necesarios para el cansat

--- nrf24/transmitter/transmitter/transmitter.ino
+++ nrf24/transmitter/transmitter/transmitter.ino
@@ -1,13 +1,15 @@
-#include <Wire.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
 #include <SPI.h>
 #include <RF24.h>
 
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME680.h>
-#include <TinyGPS++.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
-//#include <Adafruit_MPU6050.h>
+// =========================
+// WiFi + server
+// =========================
+const char* ssid = "Electronica";
+const char* password = "KIRCHHOFF24";
+const char* serverUrl = "https://cansat1.onrender.com/api/telemetry";
 
 // =========================
 // nRF24
@@ -18,273 +20,140 @@
 RF24 radio(NRF_CE, NRF_CSN);
 const byte address[6] = "GAAY1";
 
-// Max 32 bytes
+// Must match transmitter struct exactly
 struct TelemetryPacket {
   uint32_t counter;
   float lat;
   float lon;
   float alt;
-  float temp;
-  float pressure;
-  float humidity;
+  float temp;       // BME680 temperature
+  float pressure;   // BME680 pressure
+  float humidity;   // BME680 humidity
   uint8_t sat;
+  uint8_t padding[3];
 };
 
 TelemetryPacket packet;
 
-// =========================
-// GPS
-// =========================
-#define RXD2 16
-#define TXD2 17
-TinyGPSPlus gps;
-
-// =========================
-// DS18B20
-// =========================
-#define ONE_WIRE_BUS 4
-OneWire oneWire(ONE_WIRE_BUS);
-//DallasTemperature ds18b20(&oneWire);
-
-// =========================
-// BME680 + MPU6050
-// =========================
-Adafruit_BME680 bme;
-//Adafruit_MPU6050 mpu;
-
-// =========================
-// Timing
-// =========================
-unsigned long lastSendMs = 0;
-const unsigned long sendIntervalMs = 2000;
-
-// =========================
-// MPU filter
-// =========================
-/*float pitch = 0.0f;
-float roll  = 0.0f;
-float yaw   = 0.0f;
-
-unsigned long lastTimeMPU = 0;
-const float alpha = 0.96f;
-
-float accBiasX = 0, accBiasY = 0, accBiasZ = 0;
-float gyroBiasX = 0, gyroBiasY = 0, gyroBiasZ = 0;
-*/
-// Last sensor values
-//float lastTempDS18B20 = NAN;
-float lastBmeTemp = NAN;
-float lastHumidity = NAN;
-float lastPressure = NAN;
-float lastGas = NAN;
-
-/*void calibrarMPU6050();
-void getAccelAngles(float ax, float ay, float az, float &roll_rad, float &pitch_rad);
-float rad2deg(float rad, bool to360 = false);*/
-void updateGPS();
-//void updateMPU();
-void updateEnvironmentalSensors();
-void sendRadioPacket();
+void connectWiFi();
+void sendToServer(const TelemetryPacket& p);
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
+  connectWiFi();
+
   SPI.begin(18, 19, 23, NRF_CSN);
-  Wire.begin();
-  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
-
-  //ds18b20.begin();
-
-  if (!bme.begin(0x77)) {
-    Serial.println("No se encuentra el sensor BME680");
-  } else {
-    bme.setTemperatureOversampling(BME680_OS_8X);
-    bme.setHumidityOversampling(BME680_OS_2X);
-    bme.setPressureOversampling(BME680_OS_4X);
-    bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-    bme.setGasHeater(320, 150);
-    Serial.println("BME680 detectado");
-  }
-
-  /*if (!mpu.begin()) {
-    Serial.println("No se encuentra el sensor MPU6050");
-  } else {
-    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-    Serial.println("MPU6050 detectado. Calibrando...");
-    calibrarMPU6050();
-  }*/
-
 
   if (!radio.begin()) {
     Serial.println("nRF24 no detectado");
     while (1);
   }
 
-  radio.openWritingPipe(address);
+  radio.openReadingPipe(0, address);
   radio.setChannel(108);
   radio.setDataRate(RF24_250KBPS);
   radio.setPALevel(RF24_PA_LOW);
   radio.setAutoAck(true);
-  radio.stopListening();
+  radio.startListening();
 
-  packet.counter = 0;
-  //lastTimeMPU = micros();
+  Serial.print("Expected packet size: ");
+  Serial.println(sizeof(TelemetryPacket));
 
-  Serial.println("Payload listo: sensores + nRF24 TX");
+  Serial.println("Ground listo: nRF24 RX + HTTP POST");
 }
 
 void loop() {
-  updateGPS();
-  //updateMPU();
+  if (radio.available()) {
+    radio.read(&packet, sizeof(packet));
 
-  unsigned long now = millis();
-  if (now - lastSendMs >= sendIntervalMs) {
-    lastSendMs = now;
+    Serial.println("\n===== PACKET RECEIVED =====");
 
-    updateEnvironmentalSensors();
-    sendRadioPacket();
+    Serial.print("Counter: ");
+    Serial.println(packet.counter);
+
+    Serial.print("Lat: ");
+    Serial.println(packet.lat, 6);
+
+    Serial.print("Lon: ");
+    Serial.println(packet.lon, 6);
+
+    Serial.print("Alt: ");
+    Serial.println(packet.alt, 2);
+
+    Serial.print("Sat: ");
+    Serial.println(packet.sat);
+
+    Serial.print("BME Temp: ");
+    Serial.println(packet.temp, 2);
+
+    Serial.print("BME Pressure: ");
+    Serial.println(packet.pressure, 2);
+
+    Serial.print("BME Humidity: ");
+    Serial.println(packet.humidity, 2);
+
+    sendToServer(packet);
   }
 }
 
-void updateGPS() {
-  while (Serial2.available() > 0) {
-    gps.encode(Serial2.read());
-  }
-}
+void connectWiFi() {
+  Serial.print("Conectando a WiFi");
+  WiFi.begin(ssid, password);
 
-/*void updateMPU() {
-  sensors_event_t a, g, tempEvent;
-  mpu.getEvent(&a, &g, &tempEvent);
-
-  float ax = a.acceleration.x - accBiasX;
-  float ay = a.acceleration.y - accBiasY;
-  float az = a.acceleration.z - accBiasZ;
-  float gx = g.gyro.x - gyroBiasX;
-  float gy = g.gyro.y - gyroBiasY;
-  float gz = g.gyro.z - gyroBiasZ;
-
-  unsigned long now = micros();
-  float dt = (now - lastTimeMPU) / 1000000.0f;
-  lastTimeMPU = now;
-
-  if (dt <= 0 || dt > 1.0f) return;
-
-  float rollAccRad, pitchAccRad;
-  getAccelAngles(ax, ay, az, rollAccRad, pitchAccRad);
-
-  roll  = alpha * (roll  + gx * dt) + (1.0f - alpha) * rollAccRad;
-  pitch = alpha * (pitch + gy * dt) + (1.0f - alpha) * pitchAccRad;
-  yaw   = yaw + gz * dt;
-}*/
-
-void updateEnvironmentalSensors() {
-  //ds18b20.requestTemperatures();
-  //float t = ds18b20.getTempCByIndex(0);
-
- /* if (t != DEVICE_DISCONNECTED_C) {
-    lastTempDS18B20 = t;
-  }*/
-
-  if (bme.performReading()) {
-    lastBmeTemp   = bme.temperature;
-    lastHumidity  = bme.humidity;
-    lastPressure  = bme.pressure / 100.0f;
-    lastGas       = bme.gas_resistance / 1000.0f;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
 
-  Serial.println("\n===== PAYLOAD SENSOR REPORT =====");
-
-  Serial.print("GPS valid: ");
-  Serial.println(gps.location.isValid() ? "yes" : "no");
-
-  /*Serial.print("Temp DS18B20: ");
-  Serial.println(lastTempDS18B20);
-*/
-  Serial.print("Pressure: ");
-  Serial.println(lastPressure);
-
-  Serial.print("Humidity: ");
-  Serial.println(lastHumidity);
-
-  /*Serial.print("ROLL: ");
-  Serial.print(rad2deg(roll));
-  Serial.print(" | PITCH: ");
-  Serial.print(rad2deg(pitch));
-  Serial.print(" | YAW: ");
-  Serial.println(rad2deg(yaw));*/
+  Serial.println("\nWiFi conectado");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
 }
 
-void sendRadioPacket() {
-  if (!gps.location.isValid()) {
-    Serial.println("GPS no válido todavía, no se envía por nRF24");
-    return;
+void sendToServer(const TelemetryPacket& p) {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi desconectado. Reintentando...");
+    connectWiFi();
+
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("No se pudo reconectar WiFi");
+      return;
+    }
   }
 
-  packet.counter++;
-  packet.lat = gps.location.lat();
-  packet.lon = gps.location.lng();
-  packet.alt = gps.altitude.isValid() ? gps.altitude.meters() : 0.0f;
-  packet.sat = gps.satellites.isValid() ? gps.satellites.value() : 0;
+  String json = "{";
+  json += "\"lat\":" + String(p.lat, 6) + ",";
+  json += "\"lon\":" + String(p.lon, 6) + ",";
+  json += "\"alt\":" + String(p.alt, 2) + ",";
+  json += "\"sat\":" + String(p.sat) + ",";
+  json += "\"temp\":" + String(p.temp, 2) + ",";
+  json += "\"pressure\":" + String(p.pressure, 2) + ",";
+  json += "\"humidity\":" + String(p.humidity, 2);
+  json += "}";
 
-  //packet.temp = isnan(lastTempDS18B20) ? -999.0f : lastTempDS18B20;
-  packet.pressure = isnan(lastPressure) ? -999.0f : lastPressure;
-  packet.humidity = isnan(lastHumidity) ? -999.0f : lastHumidity;
+  WiFiClientSecure client;
+  client.setInsecure();
 
-  bool ok = radio.write(&packet, sizeof(packet));
+  HTTPClient http;
+  http.begin(client, serverUrl);
+  http.addHeader("Content-Type", "application/json");
 
-  Serial.print("nRF24 packet ");
-  Serial.print(packet.counter);
-  Serial.print(" size=");
-  Serial.print(sizeof(packet));
-  Serial.print(" bytes status: ");
-  Serial.println(ok ? "OK" : "FAIL");
-}
-/*
-void calibrarMPU6050() {
-  const int numLecturas = 500;
-  float sumAccX = 0, sumAccY = 0, sumAccZ = 0;
-  float sumGyroX = 0, sumGyroY = 0, sumGyroZ = 0;
+  Serial.println("Enviando JSON:");
+  Serial.println(json);
 
-  Serial.println("Mantén el MPU6050 quieto. Calibrando...");
+  int httpCode = http.POST(json);
 
-  for (int i = 0; i < numLecturas; i++) {
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
+  Serial.print("HTTP Response: ");
+  Serial.println(httpCode);
 
-    sumAccX += a.acceleration.x;
-    sumAccY += a.acceleration.y;
-    sumAccZ += a.acceleration.z;
-
-    sumGyroX += g.gyro.x;
-    sumGyroY += g.gyro.y;
-    sumGyroZ += g.gyro.z;
-
-    delay(5);
-  }*//*
-
-  accBiasX = sumAccX / numLecturas;
-  accBiasY = sumAccY / numLecturas;
-  accBiasZ = (sumAccZ / numLecturas) - 9.81f;
-
-  gyroBiasX = sumGyroX / numLecturas;
-  gyroBiasY = sumGyroY / numLecturas;
-  gyroBiasZ = sumGyroZ / numLecturas;
-
-  Serial.println("Calibración completada");
-}*/
-
-/*void getAccelAngles(float ax, float ay, float az, float &roll_rad, float &pitch_rad) {
-  roll_rad = atan2(ay, az);
-  pitch_rad = atan2(-ax, sqrt(ay * ay + az * az));
-}
-
-float rad2deg(float rad, bool to360) {
-  float deg = rad * 180.0f / PI;
-  if (to360) {
-    deg = fmod(deg + 360.0f, 360.0f);
+  if (httpCode > 0) {
+    Serial.println(http.getString());
+  } else {
+    Serial.print("Error HTTP: ");
+    Serial.println(http.errorToString(httpCode));
   }
-  return deg;
-}*/
+
+  http.end();
+}
